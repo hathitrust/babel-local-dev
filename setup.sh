5b3d9d9b3cd2f65604d34d42d8ca4c95c2c7658f
Aaron Elkiss <aelkiss@umich.edu>
2023-03-23 16:16:53 -0400

    setup script to clone needed repos

---
+++ setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "Checking out into $PWD - enter to continue, ctrl-C to abort"
+
+read
+
+git clone --recurse-submodules git@github.com:hathitrust/imgsrv
+git clone --recurse-submodules git@github.com:hathitrust/catalog
+git clone --recurse-submodules git@github.com:hathitrust/common
+git clone --recurse-submodules -b DEV-667-stage-item git@github.com:hathitrust/ht-pairtree
+git clone --recurse-submodules -b DEV-661-docker git@github.com:hathitrust/slip
+git clone --recurse-submodules -b DEV-661-docker git@github.com:hathitrust/imgsrv-sample-data
+git clone --recurse-submodules -b DEV-661-docker git@github.com:hathitrust/lss_solr_configs
+
+# Not yet covered in the apache config although maybe it was before
+# git clone git@github.com:hathitrust/pt.git
+
+# Do we need these separately?
+# git clone git@github.com:hathitrust/mdp-lib.git
+# git clone git@github.com:hathitrust/slip-lib.git
+# git clone git@github.com:hathitrust/plack-lib.git
