e0f13c1b00d10b45c6ea05fcfb42a862793f856b
Aaron Elkiss <aelkiss@umich.edu>
2023-03-27 11:50:58 -0400

    remove branch for imgsrv-sample-data
    merged https://github.com/hathitrust/imgsrv-sample-data/pull/1

--- setup.sh
+++ setup.sh
@@ -5,6 +5,7 @@ echo "Checking out into $PWD - enter to continue, ctrl-C to abort"
 read
 
 git clone --recurse-submodules git@github.com:hathitrust/imgsrv
+git clone --recurse-submodules git@github.com:hathitrust/imgsrv-sample-data
 git clone --recurse-submodules git@github.com:hathitrust/catalog
 git clone --recurse-submodules git@github.com:hathitrust/common
 git clone --recurse-submodules git@github.com:hathitrust/pt
@@ -12,7 +13,6 @@ git clone --recurse-submodules git@github.com:hathitrust/ssd
 git clone --recurse-submodules git@github.com:hathitrust/hathitrust_catalog_indexer
 git clone --recurse-submodules -b DEV-667-stage-item git@github.com:hathitrust/ht-pairtree
 git clone --recurse-submodules -b DEV-661-docker git@github.com:hathitrust/slip
-git clone --recurse-submodules -b DEV-661-docker git@github.com:hathitrust/imgsrv-sample-data
 git clone --recurse-submodules -b DEV-661-docker git@github.com:hathitrust/lss_solr_configs
 
 # Directories the web server needs to write to under /htapps/babel
