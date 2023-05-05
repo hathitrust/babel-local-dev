3406d5c77f17d6249850d575bbfb6de1c018cf17
Aaron Elkiss <aelkiss@umich.edu>
2023-05-05 14:36:50 -0400

    Add ht_users & ht_institutions entries

--- apache-cgi/auth/auth.sql
+++ apache-cgi/auth/auth.sql
@@ -1,6 +1,14 @@
 GRANT SELECT,INSERT,UPDATE,DELETE ON ht.pt_exclusivity_ng TO `mdp-lib`;
 
 --- inserts for users with elevated access
+REPLACE INTO ht.ht_users (userid, displayname, email, role, access, expires, iprestrict, identity_provider, inst_id) VALUES
+('totaluser@hathitrust.org','HathiTrust Totaluser','totaluser@hathitrust.org','staffdeveloper','total',ADD_MONTHS(CURRENT_TIMESTAMP, 60),'^.*$','https://idp.hathitrust.org/entity','hathitrust');
+
+REPLACE INTO ht.ht_users (userid, displayname, email, role, access, expires, iprestrict, identity_provider, inst_id) VALUES
+('ssdproxy@hathitrust.org','HathiTrust Ssdproxy','ssdproxy@hathitrust.org','ssdproxy','normal',ADD_MONTHS(CURRENT_TIMESTAMP, 60),'^.*$','https://idp.hathitrust.org/entity','hathitrust');
+
+REPLACE INTO ht.ht_users (userid, displayname, email, role, access, expires, iprestrict, identity_provider, inst_id) VALUES
+('ssduser@hathitrust.org','HathiTrust Ssduser','ssduser@hathitrust.org','ssd','normal',ADD_MONTHS(CURRENT_TIMESTAMP, 60),'^.*$','https://idp.hathitrust.org/entity','hathitrust');
 
 --- inserts/updates for ht_institutions
 REPLACE INTO ht.ht_institutions (inst_id, name, domain, us, enabled, entityID, allowed_affiliations) 
@@ -11,3 +19,6 @@ REPLACE INTO ht.ht_institutions (inst_id, name, domain, us, enabled, entityID, a
 
 REPLACE INTO ht.ht_institutions (inst_id, name, domain, us, enabled, entityID, allowed_affiliations) 
   VALUES ('nfb','National Federation of the Blind','nfb.org','1','1','pumex-idp','^(member)@nfb.org');
+
+REPLACE INTO ht.ht_institutions (inst_id, name, domain, us, enabled, entityID, allowed_affiliations, emergency_status)  VALUES ('etas','ETAS Example Inst','etas.example','1','1','https://idp.etas.example','^(member)@etas.example','^(member)@etas.example');
+SetEnv DEBUG auth
